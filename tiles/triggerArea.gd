a92e7a24594e91471e7478106f5c058588a37876
tomas-kynicky <kynickyt@gmail.com>
2025-11-13 18:39:30 +0100

    feat: wave system

--- tiles/triggerArea.gd
+++ tiles/triggerArea.gd
@@ -1,13 +1,26 @@
 extends Area3D
 
+@export var waitTime: int = 5
+
+var tileTimer: Timer
+
 signal playerEnterArea(index: int, body: Node3D)
 signal playerLeaveArea(index: int, body: Node3D)
+signal timeOut(timeOut: bool)
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
+	tileTimer = Timer.new()
+	tileTimer.name = "TileTimer"
+	add_child(tileTimer)
+	tileTimer.one_shot = true
+	tileTimer.wait_time = waitTime
+	if not tileTimer.timeout.is_connected(_on_timer_timeout):
+		tileTimer.timeout.connect(_on_timer_timeout)
 
 func _on_body_entered(body: Node3D) -> void:
+	tileTimer.start()
 	if not body.is_in_group("player"):
 		return
 
@@ -17,6 +30,7 @@ func _on_body_entered(body: Node3D) -> void:
 		playerEnterArea.emit(idx, body)
 
 func _on_body_exited(body: Node3D) -> void:
+	tileTimer.stop()
 	if not body.is_in_group("player"):
 		return
 
@@ -24,3 +38,6 @@ func _on_body_exited(body: Node3D) -> void:
 	if tile.has_meta("tile_index"):
 		var idx: int = tile.get_meta("tile_index")
 		playerLeaveArea.emit(idx, body)
+
+func _on_timer_timeout() -> void:
+	timeOut.emit(true)
